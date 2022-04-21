1ad200ec5bd5e3c738c1128de5dfdc359f1c06f5
Billy Veillette-Daigle <bvd86@icloud.com>
2022-04-20 22:04:02 -0400

    Updating Backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating Seeds"
+150.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: 'https://source.unsplash.com/random',
+    rating: rand(0.0...10.1)
+  )
+end
+puts "Seeds created"
